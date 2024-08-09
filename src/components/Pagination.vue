bbccd27c626d2b2ac784002093e0d4bd93e56850
dvarre <varre.dimitri@gmail.com>
2024-08-09 19:31:03 +0200

    feat(#0001) - Create pagination component

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -47,12 +47,11 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="text-center">
+  <div class="text-center pagination-container">
     <v-container>
       <v-row justify="center">
         <v-col cols="8">
           <v-container class="max-width">
-            v-model current page = {{currentPage}}
             <v-pagination
                 v-model="page"
                 :length="totalPages"
@@ -63,9 +62,27 @@ export default defineComponent({
         </v-col>
       </v-row>
     </v-container>
+    <div class="size-page-container">
+      <v-btn-toggle v-model="nbElementsPerPage" divided class="border">
+        <v-btn value="10">10</v-btn>
+        <v-btn value="50">50</v-btn>
+        <v-btn value="100">100</v-btn>
+      </v-btn-toggle>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.pagination-container {
+  display: flex;
+  flex-direction: row;
+}
 
+.size-page-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  margin-right: 2rem;
+}
 </style>
